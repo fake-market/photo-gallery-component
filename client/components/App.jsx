064a45f3b395d68522550ed3e91abe0e1166b3c0
Kevin Jacobs <frevin8r@gmail.com>
2018-05-31 12:17:16 -0700

    rendered jsx files to page

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div>
+        App rendered to page
+      </div>
+    )
+  }
+
+}
