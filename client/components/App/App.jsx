f6679c2dbbe6e2cd7134c393f4ad0c0ab0eca0ec
Kevin Jacobs <frevin8r@gmail.com>
2018-06-04 10:03:36 -0700

    added highlight to thumbnail when clicked

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      productId: 7,
+      productId: 1,
       images: [],
       profileImage: '',
       selectedImage: '',
