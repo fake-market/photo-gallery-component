c22c1c00e54ce0833ab988b7aa1a356fbe71e30a
Kevin Jacobs <frevin8r@gmail.com>
2018-06-06 13:45:49 -0700

    mock API data mirrors actual data

--- client/apis/getImages.test.js
+++ client/apis/getImages.test.js
@@ -1,16 +1,94 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import getImages from './getImages';
 
 describe('Get Images API', () => {
-    it('returns data when getImages is called', done => {
-        var mock = new MockAdapter(axios);
-        const data = { response: true };
-        mock.onGet('http://localhost:1337/products/images').reply(200, data);
+  it('returns data when getImages is called', done => {
+    var mock = new MockAdapter(axios);
+    const data = {
+      images: [
+        {
+          id: 1,
+          product_id: 1,
+          is_primary: 1,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone1.jpg'
+        },
+        {
+          id: 2,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone2.jpg'
+        },
+        {
+          id: 3,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone3.jpg'
+        },
+        {
+          id: 4,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone4.jpg'
+        },
+        {
+          id: 5,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone5.jpg'
+        },
+        {
+          id: 6,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone6.jpg'
+        },
+        {
+          id: 7,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone7.jpg'
+        },
+        {
+          id: 8,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone8.jpg'
+        },
+        {
+          id: 9,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone9.jpg'
+        },
+        {
+          id: 10,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone10.jpg'
+        },
+        {
+          id: 11,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone11.jpg'
+        },
+        {
+          id: 12,
+          product_id: 1,
+          is_primary: 0,
+          s3_url: 'https://s3-us-west-1.amazonaws.com/hrla22-ebay-fe/drone12.jpg'
+        },
+      ]
+    };
 
-        getImages.getImages(1).then(response => {
-            expect(response).toEqual(data);
-            done();
-        });
+    mock.onGet('/products/images', { params: { productId: 1 } }).reply(200, data);
+
+    axios.get('/products/images', { params: { productId: 1 } }) 
+    .then(function(response) {
+      expect(response.data).toEqual(data);
+      console.log(response.data);
+      done();
     });
+
+  });
 });
