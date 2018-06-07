6bcd4185ddb7a953524c70bce1d67af2e5be2ea2
Kevin Jacobs <frevin8r@gmail.com>
2018-06-07 11:34:15 -0700

    centered thumbnail gallery based on number of images

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -10,7 +10,7 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      productId: 1,
+      productId: 4,
       images: [],
       profileImage: '',
       selectedImage: '',
