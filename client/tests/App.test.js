42fc4feff02364bae1078a23398b313feb36407a
Kevin Jacobs <frevin8r@gmail.com>
2018-06-07 17:53:54 -0700

    added button index tests

--- client/tests/App.test.js
+++ client/tests/App.test.js
@@ -56,5 +56,88 @@ describe('App mount tests', () => {
     expect(wrapper.state().profileImage).to.equal('http://test.com');
   })
 
+
+  it('should render No Images picture if no images', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('img.noImage').length).to.equal(1);
+  })
+
+
+  it('should not render No Images picture if there are images', () => {
+    const wrapper = mount(<App />);
+    wrapper.setState({
+      images:  ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    })
+    expect(wrapper.find('img.noImage').length).to.equal(0);
+  })
+
+  it('should not render buttons if not enough images', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('a#prev').length).to.equal(0);
+    expect(wrapper.find('a#next').length).to.equal(0);
+    wrapper.setState({
+      images:  ['a', 'b', 'c', 'd', 'e']
+    })
+    expect(wrapper.find('a#prev').length).to.equal(0);
+    expect(wrapper.find('a#next').length).to.equal(0);
+  })
+
+  it('should render buttons if enough images', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('a#prev').length).to.equal(0);
+    expect(wrapper.find('a#next').length).to.equal(0);
+    wrapper.setState({
+      images:  ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    })
+    expect(wrapper.find('a#prev').length).to.equal(1);
+    expect(wrapper.find('a#next').length).to.equal(1);
+  })
+
 })
 
+describe('Buttons setting indices', () => {
+  it('should have default start and end indices', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.state('startIndex')).to.equal(0);
+    expect(wrapper.state('endIndex')).to.equal(5);
+  })
+
+  it('should update indices when previous or next are selected', () => {
+    const wrapper = mount(<App />);
+    wrapper.setState({
+      images:  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm']
+    })
+    wrapper.find('a#next').simulate('click');
+    expect(wrapper.state('startIndex')).to.equal(6);
+    expect(wrapper.state('endIndex')).to.equal(11);
+
+    wrapper.find('a#prev').simulate('click');
+    expect(wrapper.state('startIndex')).to.equal(0);
+    expect(wrapper.state('endIndex')).to.equal(5);
+
+  })
+
+  it('should not update indices if greater than length of images or negative', () => {
+    const wrapper = mount(<App />);
+    wrapper.setState({
+      images:  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    })
+    wrapper.find('a#next').simulate('click');
+    expect(wrapper.state('startIndex')).to.equal(3);
+    expect(wrapper.state('endIndex')).to.equal(8);
+
+    wrapper.find('a#next').simulate('click');
+    expect(wrapper.state('startIndex')).to.equal(3);
+    expect(wrapper.state('endIndex')).to.equal(8);
+
+    wrapper.find('a#prev').simulate('click');
+    expect(wrapper.state('startIndex')).to.equal(0);
+    expect(wrapper.state('endIndex')).to.equal(5);
+
+    wrapper.find('a#prev').simulate('click');
+    expect(wrapper.state('startIndex')).to.equal(0);
+    expect(wrapper.state('endIndex')).to.equal(5);
+
+  })
+
+})
